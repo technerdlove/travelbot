4c710930fb844689f0f368aa0f5793be0441eb5d
technerdlove <technerdlove@users.noreply.github.com>
2018-02-04 05:03:17 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-# Here I create create a basic Flask app. Flask is a web framework for Python, read more here: http://flask.pocoo.org/docs/0.12/quickstart/
+# ======= Coding Our Bot =========
+
+# Here I create a basic Flask app. 
+# Flask is a web framework for Python, read more here: http://flask.pocoo.org/docs/0.12/quickstart/
 
 from flask import Flask, request
 
@@ -15,4 +18,9 @@ if __name__ == '__main__':
 # Now, run from the command line by typing python3 app.py
 # You should see something like this:  
 # * Running on http://127.0.0.1:5000/ (Press CTRL C to quit)
+
+# If you navigate to the link given from running the app (in this example http://127.0.0.1:5000/) in a browser, 
+# you will see a page load that says “Hello World!” 
+
+# ======= From Basic Flask app to Bot =========
 
