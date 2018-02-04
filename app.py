43200a08ef8713a2f50b13636a17f18a59ade16f
technerdlove <technerdlove@users.noreply.github.com>
2018-02-04 05:09:34 -0800

    Update app.py

--- app.py
+++ app.py
@@ -40,3 +40,28 @@ if request.method == 'GET':
 # that they will use to verify the bot only responds to requests sent from Messenger. 
 
 
+# If the bot is not receiving a GET request, 
+# it is likely receiving a POST request where Facebook is sending your bot a message sent by a user.
+
+# if the request was not get, it must be POST and we can just proceed with sending a message # back to user
+   else:
+        # get whatever message a user sent the bot
+       output = request.get_json()
+       for event in output['entry']:
+          messaging = event['messaging']
+          for message in messaging:
+            if message.get('message'):
+                #Facebook Messenger ID for user so we know where to send response back to
+                recipient_id = message['sender']['id']
+                if message['message'].get('text'):
+                    response_sent_text = get_message()
+                    send_message(recipient_id, response_sent_text)
+                #if user sends us a GIF, photo,video, or any other non-text item
+                if message['message'].get('attachments'):
+                    response_sent_nontext = get_message()
+                    send_message(recipient_id, response_sent_nontext)
+    return "Message Processed"
+
+
+
+
